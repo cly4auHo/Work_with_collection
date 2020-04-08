d3e2c43a52d0d59eddfbd782b67a0a06d106f91c
Arsen <52761327+cly4auHo@users.noreply.github.com>
2020-04-08 16:33:44 +0300

    Update Test.java

--- Test.java
+++ Test.java
@@ -5,10 +5,9 @@ import java.util.Collections;
 import java.util.Arrays;
 
 public class Test {
-
     public static void main(String[] args) {
 
-        ArrayList<Double> list = new ArrayList<>(Arrays.asList(5.0, 5.9, 10.9, 0.0, -9.8));
+        List<Double> list = new ArrayList<>(Arrays.asList(5.0, 5.9, 10.9, 0.0, -9.8));
 
         Collections.sort(list);
         System.out.println(list);
